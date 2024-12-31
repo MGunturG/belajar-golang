f03172b8acd761e388b1d67c8fea21283bdec41e
Guntur <39583921+MGunturG@users.noreply.github.com>
2024-12-31 11:07:59 +0700

    tambah materi array

---
+++ array/main.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+func main() {
+	// contoh penerapan array
+	var names [4]string
+	names[0] = "trafalgar"
+	names[1] = "d"
+	names[2] = "water"
+	names[3] = "law"
+
+	fmt.Println(names[0], names[1], names[2], names[3])
+	// output : trafalgar d water law
+
+	// contoh inisialisasi nilai awal array
+	var array_buah = [4]string{"apel", "anggur", "pisang", "melon"}
+	fmt.Println("Jumlah elemen array \t\t", len(array_buah))
+	fmt.Println("Isi arraynya \t", array_buah)
+	
+	// output :
+	// Jumlah elemen array	4
+	// Isi arraynya		[apel anggur pisang melon]
+	
+	// inisialisasi juga bisa seperti ini
+	// untuk inisialisasi seperti di bawah ini
+	// tanda baca koma (,) harus diassign di setiap
+	// akhir elemen
+	var array_mobil = [4]string{
+		"toyota",
+		"honda",
+		"nissan",
+		"lexus",
+	}
+
+
+	// Inisialisasi array tanpa jumlah elemen array
+	// kalo sebelumnya kita harus ngasih tau jumlah elemen arraynya
+	// misal : [4]string, berarti ada 4 string di dalam arraynya
+	// nah, kita juga bisa inisiasi array tanpa assign jumlah elemen arraynya
+
+	var array_angka = [...]int{1,2,3,4}
+	fmt.Println("Data array \t:", array_angka)
+	fmt.Println("Jumlah elemennya \t:", len(array_angka))
+
+	// secara otomatis, compiler akan meng-assign jumlah elemen array angka
+	// sebanyak 4 elemen. bisa dilihat dari outputnya:
+	// Data array		: [1 2 3 4]
+	// Jumlah elemennya	: 4
+	
+
+	// Array Multidimensi
+	// > oh shit, here we go again. my first nightmare from learning C
+
+	
+}
